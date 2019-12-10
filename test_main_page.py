ab1a04c01fa22902937964a06e1743f46bf2ad79
swad <swad2007@yandex.ru>
2019-12-10 10:26:47 +0300

    Add base and base page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,14 @@
+#pytest -v --tb=line --language=en test_main_page.py
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    go_to_login_page(browser)
+
+
